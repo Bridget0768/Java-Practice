3765c2afb998d2f7d2ad47d86f0220296596e019
Bridget0768 <bridgetkamusiime01@gmail.com>
2026-09-02 20:56:50 +0300

    Add files via upload

---
+++ numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class numbers {
+   public static void main(String[]args){
+    double num1,num2,num3,num4,num5;
+double average;
+Scanner input = new Scanner(System.in);
+
+
+System.out.println("Enter the first number:");
+num1 = input.nextDouble();
+
+System.out.println("Enter the second number:");
+num2 =input. nextDouble();
+
+System.out.println("Enter the third number:");
+num3 = input.nextDouble();
+
+System.out.println("Enter the fourth number:");
+num4 = input. nextDouble();
+
+System.out.println("Enter the fifth number:");
+num5 = input.nextDouble();
+
+
+
+   } 
+}
